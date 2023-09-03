1806b9020395143282cc540ef23c0451afab3623
Mahammad <mahammad.code@gmail.com>
2023-09-04 01:41:51 +0400

    social-media-canvas/complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
-import { TiThMenuOutline } from '../../node_modules/react-icons/ti'
 import OffCanvasLeft from './home/OffCanvasLeft';
 const Header = () => {
-    // const [currentPage, setCurrentPage] = useState("");
-    const pathName = window.location.pathname;
-    const [searchOpen , setSearchOpen]  = useState(false);
-    // useEffect(() => {
-    //     switch (pathName) {
-    //         case '/':
-    //             setCurrentPage(1)
-    //             break;
-
-    //         case '/shop':
-    //             setCurrentPage(2)
-    //             break;
-
-    //         case '/blog':
-    //             setCurrentPage(3)
-    //             break;
-
-    //         case '/events':
-    //             setCurrentPage(4)
-    //             break;
-
-    //         case '/contact':
-    //             setCurrentPage(5)
-    //             break;
-
-    //         default:
-    //             setCurrentPage(1)
-    //             break;
-    //     }
-    // }, [])
-
+    
+    const [searchOpen, setSearchOpen] = useState(false);
+    const [socialMediaOpen , setSocialMediaOpen] = useState(false);
+    
     return (
         <>
             <div className="navbar-container">
@@ -110,11 +82,11 @@ const Header = () => {
                                     <i className="fa-solid fa-bag-shopping"></i>
                                 </div>
 
-                                <div className="user-tool" onClick={()=>setSearchOpen(true)}>
+                                <div className="user-tool" onClick={() => setSearchOpen(true)}>
                                     <i className="fa-solid fa-magnifying-glass"></i>
                                 </div>
 
-                                <div className="user-tool d-none d-sm-none d-md-block">
+                                <div className="user-tool d-none d-sm-none d-md-block" onClick={()=>setSocialMediaOpen(true)}>
                                     <i className="fa-solid fa-bars"></i>
                                 </div>
 
@@ -130,21 +102,57 @@ const Header = () => {
             </div>
 
 
-
+            {/* Search Canvas  */}
             <div className={`search-container ${searchOpen ? "search-box-show" : ""}`}>
                 <div className="container">
 
                     <div className="head">
                         <img src="https://hotcoffee.themerex.net/wp-content/uploads/2021/11/logo-dark.png" alt="" />
 
-                        <i className="fa-solid fa-xmark" onClick={()=>setSearchOpen(false)}></i>
+                        <i className="fa-solid fa-xmark" onClick={() => setSearchOpen(false)}></i>
                     </div>
 
                     <div className="search-input">
                         <div class="input-group">
                             <input type="text" class="form-control" placeholder="Search ..." aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                                <span class="input-group-text" id="basic-addon2"><i className="fa-solid fa-magnifying-glass"></i></span>
+                            <span class="input-group-text" id="basic-addon2"><i className="fa-solid fa-magnifying-glass"></i></span>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={`social-media-canvas ${socialMediaOpen ? "social-media-show" : ""}`}>
+                <div className="social-media-container">
+                    <div className="head">
+                        <img src="https://hotcoffee.themerex.net/wp-content/uploads/2021/11/logo-dark.png" alt="" />
+                        <i className="fa-solid fa-xmark" onClick={()=>setSocialMediaOpen(false)}></i>
+                    </div>
+
+                    <div className="media-accounts">
+                        <div className="item">
+                            <i className='fa-brands fa-facebook-f'></i>
+                            <p>Facebook</p>
+                        </div>
+
+                        <div className="item">
+                            <i className='fa-brands fa-twitter'></i>
+                            <p>Twitter</p>
+                        </div>
+
+                        <div className="item">
+                            <i class="fa-brands fa-dribbble"></i>
+                            <p>Dribble</p>
+                        </div>
+
+                        <div className="item">
+                            <i className='fa-brands fa-instagram'></i>
+                            <p>Instagram</p>
+                        </div>
+                    </div>
+
+                    <div className="contact-info">
+                        <p>+1 840 841 25 69</p>
+                        <span>info@email.com</span>
                     </div>
                 </div>
             </div>
